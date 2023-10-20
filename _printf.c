0be22abba008dd6af07fdc314a43f0d82bda7dc0
iammakena@gmail.com <root@MAKENA>
2023-10-21 01:17:49 +0300

    Fix conflicts

--- _printf.c
+++ _printf.c
@@ -9,7 +9,8 @@ int _printf(const char *format, ...)
 {
 		Specifiers Specify[] = {
 		{"%c", characters}, {"%%", percent}, {"%s", string}, {"%d", case_d},
-		{"%i", case_i}, {"%b", binary},
+		{"%i", case_i}, {"%b", binary}, {"%u", case_u}, {"%o", base_eight}, {"%x", hexalower},
+		{"%X", hexaupper},
 		};
 	va_list p;
 	int chars, specifierFound = 0;
