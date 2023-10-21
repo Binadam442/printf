3e275ac48404aa6defc4f88fa278815b1ca40813
iammakena@gmail.com <root@MAKENA>
2023-10-21 10:39:05 +0300

    Task 0

--- _printf.c
+++ _printf.c
@@ -9,8 +9,6 @@ int _printf(const char *format, ...)
 {
 		Specifiers Specify[] = {
 		{"%c", characters}, {"%%", percent}, {"%s", string},
-		{"%d", case_d}, {"%i", case_i}, {"%b", binary}, {"%u", case_u},
-		{"%0", base_eight}, {"%x", hexalower}, {"%X", hexaupper},
 		};
 	va_list p;
 	int chars, specifierFound = 0;
